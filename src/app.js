363e2d360402698d416c423915ea4856de2f3222
takashi <takashioicsn@gmail.com>
2019-07-02 01:14:08 +0900

    modify app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
-const count = document.getElementById('count');
+const isbn = document.getElementById("isbn");
 
-// 加算
-document.getElementById('plusBtn').addEventListener('click', () => ++count.value);
-
-// 減算
-document.getElementById('minusBtn').addEventListener('click', () => {
-  if (count.value > 0) --count.value;
+document.getElementById("search").addEventListener("click", () => {
+  fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn.value}`).then(response => {
+    if(response.ok) {
+      response.json().then(book => {
+        console.log(book);
+      })
+    }
+    else {
+      console.error("サーバーエラー")
+    }
+  }).catch(error => {
+    console.error("ネットワークエラー")
+  });
 });
-
-// リセット
-document.getElementById('resetBtn').addEventListener('click', () => count.value = 0);
