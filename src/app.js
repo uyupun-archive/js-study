ece773ccebe73db5e4bf3c311531a5ac08e810c4
Kazuki Tobita <kazukiti201@gmail.com>
2019-07-05 02:11:27 +0900

    Modify sample program

--- src/app.js
+++ src/app.js
@@ -1,25 +1,25 @@
-const isbn = document.getElementById("isbn");
-const main = document.getElementsByTagName("main")[0];
-let books = [];
+const books = [];
 
-// 本の検索
+// 書籍の検索
 const searchBook = () => {
+  const isbn = document.getElementById("isbn");
   fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn.value}`).then(response => {
     if(response.ok) {
       response.json().then(book => {
-        showBook(book.items[0].volumeInfo);
+        showSearchResult(book.items[0].volumeInfo);
       });
     } else {
-      console.error("サーバーエラー")
+      console.error("Server Error!")
     }
   }).catch(error => {
-    console.error("ネットワークエラー")
+    console.error("Network Error!")
   });
 };
 
 // 検索結果の表示
-const showBook = book => {
+const showSearchResult = book => {
   let result = document.getElementById("result");
+  result.textContent = null;
 
   if (book.totalItems === 0) {
     result.appendChild(document.createElement("p")).appendChild(document.createTextNode("本が見つかりませんでした。"));
@@ -32,12 +32,26 @@ const showBook = book => {
     result.appendChild(document.createElement("div")).appendChild(document.createTextNode(`著者: ${authors}`));
     result.appendChild(document.createElement("div")).appendChild(document.createTextNode(description));
 
-    const button = document.createElement("button");
-    button.setAttribute("type", "button");
-    button.setAttribute("id", "add-book");
-    result.appendChild(button).appendChild(document.createTextNode("追加"));
+    const button = makeButton("add-book", "追加");
+    result.appendChild(button);
 
     document.getElementById("add-book").addEventListener("click", () => addBook(book));
+  }
+};
+
+// 本棚の表示
+const showBookshelf = () => {
+  const bookshelf = document.getElementById("bookshelf");
+  bookshelf.textContent = null;
+  for (let [i, book] of books.entries()) {
+    const li = document.createElement("li");
+    const button = makeButton(`delete-book-${i}`, "削除");
+
+    const tmp = bookshelf.appendChild(li);
+    tmp.appendChild(document.createTextNode(`${book.title}: ${book.authors}`));
+    tmp.appendChild(button);
+
+    document.getElementById(`delete-book-${i}`).addEventListener("click", () => deleteBook(i));
   }
 };
 
@@ -47,29 +61,19 @@ const addBook = book => {
   showBookshelf();
 };
 
-// 本棚の表示
-const showBookshelf = () => {
-  const bookshelf = document.getElementById("bookshelf");
-  bookshelf.textContent = null;
-  for (let [i, book] of books.entries()) {
-    const li = document.createElement("li");
-
-    const button = document.createElement("button");
-    button.setAttribute("type", "button");
-    button.setAttribute("id", `delete-book-${i}`);
-
-    const tmp = bookshelf.appendChild(li);
-    tmp.appendChild(document.createTextNode(`${book.title}: ${book.authors}`));
-    tmp.appendChild(button).appendChild(document.createTextNode("削除"));
-
-    document.getElementById(`delete-book-${i}`).addEventListener("click", () => deleteBook(i));
-  }
-};
-
 // 本の削除
 const deleteBook = i => {
   books.splice(i + 1, 1);
   showBookshelf();
+};
+
+// ボタンの作成
+const makeButton = (id, text) => {
+  const button = document.createElement("button");
+  button.setAttribute("type", "button");
+  button.setAttribute("id", id);
+  button.appendChild(document.createTextNode(text));
+  return button;
 };
 
 document.getElementById("search").addEventListener("click", searchBook);
